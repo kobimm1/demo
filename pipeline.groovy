d8f69ad7754998f20c14b8a793b40a656c34b27f
kobimm1 <52923025+kobimm1@users.noreply.github.com>
2022-01-28 12:48:57 +0200

    Update pipeline.groovy
    add stage

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,9 +1,13 @@
 pipeline {
     agent {
-        checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/kobimm1/demo.git']]])
         docker { image 'python' }
     }
     stages {
+        stage('init'){
+            steps {
+               checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/kobimm1/demo.git']]])
+            }
+        }
         stage('Test') {
             steps {
                 sh 'python demo.py'

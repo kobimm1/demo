51dcbc72de2a643a858bafd08181108067ee9b32
kobimm1 <52923025+kobimm1@users.noreply.github.com>
2022-01-28 12:45:55 +0200

    Update pipeline.groovy
    add checkout

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,11 +1,12 @@
 pipeline {
     agent {
+        checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/kobimm1/demo.git']]])
         docker { image 'python' }
     }
     stages {
         stage('Test') {
             steps {
-                sh 'node --version'
+                sh 'python demo.py'
             }
         }
     }

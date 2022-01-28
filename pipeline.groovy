f38f04ede52efb77cb57768c3233eec0ca993cce
kobimm1 <52923025+kobimm1@users.noreply.github.com>
2022-01-28 12:39:48 +0200

    Create pipeline.groovy
    add pipeline.groovy

---
+++ pipeline.groovy
@@ -0,0 +1,12 @@
+pipeline {
+    agent {
+        docker { image 'python' }
+    }
+    stages {
+        stage('Test') {
+            steps {
+                sh 'node --version'
+            }
+        }
+    }
+}
